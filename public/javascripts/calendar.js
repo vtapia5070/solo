6fb580ed2b20d53e265324376d975406573e0ff1
Victoria <tapia.victoria5070@gmail.com>
2015-09-22 00:31:30 -0700

    transferred calendar services into services file

--- public/javascripts/calendar.js
+++ public/javascripts/calendar.js
@@ -1,4 +1,4 @@
-var app = angular.module('calendarApp', [])
+var app = angular.module('calendarApp', ['calendar.services'])
 
   .controller('headerController', function($scope, getCurrent, makeCal){
     $scope.dateinfo = {
@@ -22,72 +22,4 @@ var app = angular.module('calendarApp', [])
         }
       }
     };
-  })
-
-  .service('getCurrent', function(){
-    var date = new Date;
-    var months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    return {
-      getMonth: function(){
-        return months[date.getMonth()];
-      },
-      getYear: function(){
-        return date.getFullYear();
-      },
-      getDay: function(){
-        date.getDate();
-      }
-    }
-  })
-
-  .service('makeCal', function(){
-    var names = ['Sun','Mon','Tue','Wed','Thurs','Fri','Sat'];
-    var date = new Date();
-    var today = date.getDate();
-    var generateCal = function () {
-      var rows = [[]];
-      var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-      var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-      var firstDayName = firstDay.toDateString().split(" ");
-      function setRow (num, count) {
-        count = count || 0;
-        var flag = false;
-        var row = [];
-        if (num >= 1) {
-          for (var i = 0; i < names.length; i++) {
-            count++;
-            if (count > lastDay.getDate()) {
-              row.push('');
-              flag = true;
-            } else 
-            row.push(count);
-          }
-          rows.push(row);
-          if (!flag) {          
-            setRow(num++, count);
-          } else {
-            return;
-          }
-        }
-      }
-      var count = 0;
-      for (var i = 0; i < names.length; i++){
-        if (names[i] === firstDayName[0] && count === 0) {
-          count++;
-          rows[0].push(count);
-        } else if (count === 0) {
-          rows[0].push("");
-        } else {
-          count++;
-          rows[0].push(count);
-        }
-      }
-      setRow(1, count);
-      return rows;
-    };
-    return {
-      setRows : generateCal(),
-      names: names,
-      today: today
-    }
   })
---
+++ public/javascripts/schedule.js
@@ -0,0 +1,5 @@
+var app = angular.module('scheduleApp', [])
+
+.controller('scheduleController', function ($scope) {
+  
+})
